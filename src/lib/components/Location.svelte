99455cbdac9bc363f0e612452cabc0c5654698ba
campbell-frost <campbellsfrost@gmail.com>
2024-11-03 19:23:10 -0500

    card changes

--- src/lib/components/Location.svelte
+++ src/lib/components/Location.svelte
@@ -4,15 +4,15 @@
 </script>
 
 <div class="card">
-	<div class="flex flex-col items-center  lg:gap-12">
-		<div class="flex-1  text-center lg:text-left">
-			<h1 class="title">
+	<div class="flex flex-col items-center ">
+		<div class="flex-1 text-center lg:text-left">
+			<h1 class="title pb-10">
 				{title}
 			</h1>
 		</div>
 		<div class="flex w-full flex-col justify-center">
 			<img src={image} alt={title} class="h-full w-full object-cover transition-all duration-300 rounded-3xl mb-4" />
-			<p class="text-lg leading-relaxed text-gray-500">
+			<p class="text-lg leading-relaxed text-gray-500 dark:text-gray-400">
 				{footer}
 			</p>
 		</div>
